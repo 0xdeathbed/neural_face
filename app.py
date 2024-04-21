7be2d84be64eaed3ab803fcff2d5e41b9a2f8e53
Paras <161898004+paradoxedmind@users.noreply.github.com>
2024-04-21 10:47:13 +0530

    Implemented command line program

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import argparse
 import torch
 from detector import FaceDetector
 from recognizer import FaceRecognizer
+from attribute import FaceAttribute
 from PIL import ImageDraw, Image
 import os
 import shutil
@@ -17,11 +18,16 @@ parser.add_argument("--register", action="store_true",
 parser.add_argument("--cpu", action="store_true", help="Explicitly use cpu")
 parser.add_argument("-f", action="store", help="Path to an image")
 parser.add_argument("--detect", action="store_true",
-                    help="Detect face in image")
+                    help="Detect face in image [-f should point to valid img]")
 parser.add_argument("--save", action="store_true",
                     help="Save detected or recognized image in current directory")
 parser.add_argument("--recognize", action="store_true",
                     help="Recognize unknown face from registered faces")
+parser.add_argument("--attribute", action="store_true",
+                    help="Estimate age and gender of face in image")
+parser.add_argument("--live", action="store_true", help="To enable live cam")
+parser.add_argument("-l", action="store", default="detect", choices=["detect", "recognize", "attribute"],
+                    help="Which feature to use for live cam [to be used with --live arg]")
 
 args = parser.parse_args()
 
@@ -67,8 +73,10 @@ def register_face(file):
     exit(0)
 
 
-def recognize_face(file):
+def recognize_face(file, attribute=False):
     recognizer = FaceRecognizer(device=device)
+    if attribute:
+        attribute_detector = FaceAttribute()
 
     print("Loading Image")
     image = Image.open(file)
@@ -77,9 +85,15 @@ def recognize_face(file):
     details = recognizer.recognize(image)
 
     for name, min_dist, box in details:
+        attribute_text = ""
+        if attribute:
+            age, gender = attribute_detector.detect_age_gender(
+                image=frame, box=box)
+
+            attribute_text = f"{gender},{age}"
 
         x1, y1, x2, y2 = box
-        text = f"{name},{min_dist:.2f}"
+        text = f"{name},{min_dist:.2f} [{attribute_text}]"
 
         cv2.putText(frame,
                     text, (x1, y1 - 5), cv2.FONT_HERSHEY_DUPLEX, 0.9, (0, 255, 0), 1)
@@ -96,17 +110,158 @@ def recognize_face(file):
         image.show()
 
 
+def attribute(file):
+    detector = FaceDetector(model='mtcnn')
+    attribute_detector = FaceAttribute()
+
+    print("Loading Image")
+    frame = cv2.imread(file)
+
+    boxes = detector.detect_face(frame)
+
+    if boxes is not None:
+        for box in boxes:
+            age, gender = attribute_detector.detect_age_gender(
+                image=frame, box=box)
+
+            x1, y1, x2, y2 = box
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 225, 0), 2)
+
+            cv2.putText(frame, f"{gender},{age}", (x1, y1 - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2, cv2.LINE_AA)
+
+    image = Image.fromarray(frame)
+
+    if args.save:
+        save_img = f"attribute_{file}"
+        print(f"Saved detected attribute image as: {save_img}")
+        image.save(save_img)
+    else:
+        print("Showing recgonized image")
+        image.show()
+
+
+def live_detect(video_src=0):
+    detector = FaceDetector(model="mtcnn")
+
+    vid = cv2.VideoCapture(video_src)
+    if vid is None:
+        exit(0)
+
+    while vid.grab():
+        _, img = vid.retrieve()
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.flip(img, 1)
+
+        boxes = detector.detect_face(img)
+
+        if boxes is not None:
+            for box in boxes:
+                x1, y1, x2, y2 = box
+                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 225, 0), 2)
+
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        cv2.imshow("window", img)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    vid.release()
+    cv2.destroyAllWindows()
+
+
+def live_recognize(video_src=0):
+    recognizer = FaceRecognizer(device=device)
+
+    cam = cv2.VideoCapture(video_src)
+    if cam is None:
+        exit(0)
+
+    while cam.grab():
+        _, frame = cam.retrieve()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = cv2.flip(frame, 1)
+
+        img = Image.fromarray(frame)
+
+        details = recognizer.recognize(img)
+
+        for name, min_dist, box in details:
+
+            x1, y1, x2, y2 = box
+
+            cv2.putText(frame,
+                        f"{name} {min_dist:.3f}", (x1, y1 - 5), cv2.FONT_HERSHEY_DUPLEX, 0.9, (0, 255, 0), 1)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        cv2.imshow("__", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    cam.release()
+    cv2.destroyAllWindows()
+
+
+def live_attribute(video_src=0):
+    detector = FaceDetector(model='mtcnn')
+    attribute_detector = FaceAttribute()
+
+    vid = cv2.VideoCapture(video_src)
+    if vid is None:
+        exit(0)
+
+    while vid.grab():
+        _, img = vid.retrieve()
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.flip(img, 1)
+
+        boxes = detector.detect_face(img)
+
+        if boxes is not None:
+            for box in boxes:
+                box = list(map(int, box))
+
+                age, gender = attribute_detector.detect_age_gender(
+                    image=img, box=box)
+
+                x1, y1, x2, y2 = box
+                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 225, 0), 2)
+
+                cv2.putText(img, f"{gender},{age}", (x1, y1 - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2, cv2.LINE_AA)
+
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        cv2.imshow("window", img)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    vid.release()
+    cv2.destroyAllWindows()
+
+
 if __name__ == '__main__':
     if args.cpu:
         device = "cpu"
     print("Running on device: {}".format(device))
 
-    if args.f is not None:
+    if args.live:
+        if args.l == 'detect':
+            live_detect()
+        elif args.l == 'recognize':
+            live_recognize()
+        elif args.l == 'attribute':
+            live_attribute()
+        else:
+            print("Invalid value: -l detect|recognize|attribute")
+    elif args.f is not None:
         if args.recognize:
-            recognize_face(args.f)
-
+            recognize_face(args.f, args.attribute)
+        elif args.attribute:
+            attribute(args.f)
         elif args.detect:
             detection(args.f)
-
         elif args.register:
             register_face(args.f)
